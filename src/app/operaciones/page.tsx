8a0ed25cd864065ce74f87b7eecb831b1abab02e
LynnDBZ <mialynnbu22@gmail.com>
2026-04-23 23:32:45 -0500

    Seccion III finalizada

--- src/app/operaciones/page.tsx
+++ src/app/operaciones/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import Seccion1 from './Seccion1';
 import Seccion2 from './Seccion2';
+import Seccion3 from './Seccion3';
 
 export default function PaginaOperaciones() {
   const [paso, setPaso] = useState(1);
@@ -10,11 +11,13 @@ export default function PaginaOperaciones() {
   // Navegar entre secciones
   const irASiguiente = () => setPaso(paso + 1);
   const irAAnterior = () => setPaso(paso - 1);
+  const irAPago = () => console.log("Yendo a la pasarela de pago...");
 
   return (
     <main>
       {paso === 1 && <Seccion1 alSiguiente={irASiguiente} />}
       {paso === 2 && <Seccion2 alAnterior={irAAnterior} alSiguiente={irASiguiente} />}
+      {paso === 3 && <Seccion3 alAnterior={irAAnterior} alPago={irAPago} />}
     </main>
   );
 }
